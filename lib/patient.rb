505496db8e7a018451d43885db033ee580806524
Chaim Reisman <crreisman@gmail.com>
2018-02-11 16:56:50 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,7 +1,12 @@
 class Patient
-  attr_accessor :name 
+  attr_accessor :name, :appointments 
   
   def initialize(name)
     @name = name 
+    @appointments = []
   end 
+  
+  def add_appointment(appt)
+    @appointments << appt 
+  end
 end 
